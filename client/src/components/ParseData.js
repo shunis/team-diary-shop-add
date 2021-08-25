f87c5057c1bea3bc2baf3f3e30f787c97771d926
ilgwang <maddux3154@gmail.com>
2021-08-25 16:48:52 +0900

    version 1.1.9 add format

---
+++ client/src/components/ParseData.js
@@ -0,0 +1,19 @@
+import moment from "moment";
+
+//* 날짜 포맷
+export function dateFormat(requestValue, format = "YYYY-MM-DD") {
+  if (!requestValue) return "-";
+  return moment(requestValue).locale("ko").format(format);
+}
+
+//* 날짜 + 시간 포맷
+export function dateTimeFormat(requestValue, format = "YYYY-MM-DD HH:mm:ss") {
+  if (!requestValue) return "-";
+  return moment(requestValue).locale("ko").format(format);
+}
+
+//* 숫자 천 단위 ,
+export function numberWithComma(requestValue) {
+  if (requestValue !== 0 && !requestValue) return "";
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
