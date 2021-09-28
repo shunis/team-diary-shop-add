a81f08607ead173f8b56764662d32d2bb9f684bc
ilgwang <maddux3154@gmail.com>
2021-09-28 17:57:28 +0900

    dev 푸시 안되도록

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://teamdiary:1111@teamdiary.dqpbn.mongodb.net/team_diary?retryWrites=true&w=majority",
-    // mongoURI: "mongodb+srv://park:abcd1234@parkshunis.fuyea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  };
